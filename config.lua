b63b588d27f8380d7d322eb10d5277f7504e126e
0pax <64065031+0pax@users.noreply.github.com>
2021-12-16 18:51:53 +0100

    Add new files
    Optimize, add support for new and old ESX and drop MySQL queries and client script.

---
+++ config.lua
@@ -0,0 +1,10 @@
+Config = {
+    StarterItems = {
+        {databasename = 'bread', count = 1},
+        {databasename = 'water', count = 2},
+    },
+
+    OldESX = false,
+
+    Config.NotificationSystem = 'ESX' -- or mythic_notify
+}
